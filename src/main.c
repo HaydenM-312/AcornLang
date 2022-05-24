3874820001f8dc52a67f8dc1ecb7d5160cdf267f
HaydenM-312 <hjrmg565@gmail.com>
2022-05-23 21:57:24 -0400

    Added a terminal parser

--- src/main.c
+++ src/main.c
@@ -1,15 +1,31 @@
 #include "main.h"
 
 int main(int argc, char* argv[]) {
-	char* file = fread_to_buffer(argv[1]);
-	token_list* tokenl = parse_to_token(file);
-	ast_node AST = parse(*tokenl);
+	if (argc > 1) {	
+		char* file = fread_to_buffer(argv[1]);
+		token_list* tokenl = parse_to_token(file);
+		ast_node AST = parse(*tokenl);
 
-	print_ast(AST, 0);
+		print_ast(AST, 0);
 
-	free_token_list(tokenl);
-	free_node(&AST);
-	free(file);
+		free(file);
+		free_node(&AST);
+		free_token_list(tokenl);
+	} else {
+		while (1) {
+			char input[256];
+			printf("Acorn> ");
+			fgets(input, 256, stdin);
+
+			token_list* tokenl = parse_to_token(input);
+			ast_node AST = parse(*tokenl);
+
+			print_ast(AST, 0);
+
+			free_node(&AST);
+			free_token_list(tokenl);
+		}
+	}
 	
 	return 0;
 }
