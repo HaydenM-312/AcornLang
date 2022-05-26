02edbc2bd7403d145da45cf3747743206bac52b0
HaydenM-312 <hjrmg565@gmail.com>
2022-05-26 08:11:07 -0400

    Abbreviate debug decl.

--- include/parse.h
+++ include/parse.h
@@ -9,6 +9,6 @@ ast_node 	parse_token		(ast_node*, ast_node, int*, ast_node**);
 ast_node	parse_expr		(ast_node);
 int			is_higher_pres	(int, int);
 
-char*		name_token		(int t);
-void		print_token		(token t);
-void		print_ast		(ast_node tree, int depth);
+char*		name_token		(int);
+void		print_token		(token);
+void		print_ast		(ast_node);
