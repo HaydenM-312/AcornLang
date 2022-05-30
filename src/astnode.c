144da63fe41799966141e8fd0ced57a994b01ef5
HaydenM-312 <hjrmg565@gmail.com>
2022-05-30 15:25:28 -0400

    Fixed a memory leak

--- src/astnode.c
+++ src/astnode.c
@@ -9,22 +9,24 @@ ast_node generate_node(token t) {
 	return AST;
 }
 
-void append_node(ast_node* AST, ast_node node) {
-	AST->children = realloc(AST->children, ++(AST->child_count) * sizeof(ast_node));
-	if (AST->children == NULL) error(2, NULL);
-	AST->children[AST->child_count - 1] = node;
+void append_node(ast_node* this, ast_node node) {
+	this->children = realloc(this->children, ++(this->child_count) * sizeof(ast_node));
+	if (this->children == NULL) error(2, NULL);
+	this->children[this->child_count - 1] = node;
 }
 
-void change_node(ast_node* AST, int position, ast_node new_node) {
-	AST->children[position] = new_node;
+void change_node(ast_node* this, int position, ast_node new_node) {
+	this->children[position] = new_node;
 }
 
-void remove_node(ast_node* AST, int position) {
-	for (int i = position + 1; i < AST->child_count; i++) AST->children[i-1] = AST->children[i];
-	AST->children = realloc(AST->children, --(AST->child_count) * sizeof(ast_node));
+void remove_node(ast_node* this, int position) {
+	for (int i = position + 1; i < this->child_count; i++) this->children[i-1] = this->children[i];
+	this->children = realloc(this->children, --(this->child_count) * sizeof(ast_node));
 }
 
-void free_node(ast_node* AST) {
-	if ((AST->node.type == TT_STRING || AST->node.type == TT_ID) && AST->node.data != NULL) free(AST->node.data);
-	if (AST->children != NULL) free_node(AST->children);
+void free_node(ast_node* this) {
+	if (this->node.data != NULL) free(this->node.data);
+	if (this->children != NULL) {
+		for (int i = 0; i < this->child_count; i++) free_node(&(this->children[i]));
+	}
 }
