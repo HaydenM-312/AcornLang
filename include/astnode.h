cb659d7027e5a96b95adebb554a028aca487c0e2
HaydenM-312 <hjrmg565@gmail.com>
2022-06-03 14:32:29 -0400

    Added context tree

--- include/astnode.h
+++ include/astnode.h
@@ -4,14 +4,32 @@
 #include "lexer.h"
 
 typedef struct __abstract_syntax_tree {
-	token	 node;
 	struct __abstract_syntax_tree* 
 			children;
+	token	node;
 	int 	child_count;
 } ast_node;
 
-ast_node	generate_node	(token);
-void		append_node		(ast_node*, ast_node);
-void		change_node		(ast_node*, int, ast_node);
-void		remove_node		(ast_node*, int);
-void		free_node		(ast_node*);
+typedef struct __context_tree {
+	struct __context_tree* 
+			children;
+	size_t 	size;
+
+	int 	type;
+	void* 	data;
+
+	int 	line;
+	int 	column;
+} context_tree;
+
+	ast_node		
+		ast_generate_node	(token);
+void	ast_append_node		(ast_node*, ast_node);
+void	ast_change_node		(ast_node*, int, ast_node);
+void	ast_remove_node		(ast_node*, int);
+void	ast_free_node		(ast_node*);
+
+	context_tree	
+		ctr_new_node		(int, void*, int, int);
+void	ctr_append_node		(context_tree*, context_tree);
+void	ctr_free_node		(context_tree*);
--- include/parse.h
+++ include/parse.h
@@ -9,6 +9,10 @@ ast_node 	parse_token		(ast_node*, ast_node, int*, ast_node**);
 ast_node	parse_expr		(ast_node);
 int			is_higher_pres	(int, int);
 
+	context_tree
+			contextualize	(ast_node);
+
 char*		name_token		(int);
 void		print_token		(token);
 void		print_ast		(ast_node);
+void		print_ctr		(context_tree);
